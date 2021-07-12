e7989c8c09920eb228f3a75da5faeff8fc445c07
Firq <firelp42@gmail.com>
2021-07-12 17:58:31 +0200

    Changed: - Gitignore ignores json in main directory - Added saving to json from data

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ import urllib.request
 
 with urllib.request.urlopen("https://api.atlasacademy.io/export/NA/nice_servant.json") as url:
     data = json.loads(url.read().decode())
-    print(data)
+
+with open("servant_data_nice.json", "w") as write_file:
+    json.dump(data, write_file)
