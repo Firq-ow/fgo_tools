d29eac800223e0268549847eafcfccd1b7a22118
Firq <firelp42@gmail.com>
2022-06-11 11:54:55 +0200

    # Changes to ui_prototype.py
    ## Changes
    
    - Prototype Calculation works

--- damage_calculator/ui_prototype.py
+++ damage_calculator/ui_prototype.py
@@ -2,6 +2,9 @@
 UI for damage Calculator
 """
 import PySimpleGUI as psg
+from calc_dmg import CalculateDamage
+from get_atlas_json import AtlasFunctions
+from get_atlas_json import DateError, HashError
 
 
 # Layout of main windows
@@ -10,10 +13,18 @@ layout_main = [
         psg.Text("Enter Damage formular here:")
     ],
     [
-        psg.In(size=(25, 1), enable_events=False, key="-DMG_Input-")
+        psg.In(size=(25, 1),
+               enable_events=False,
+               key="-DMG_Input-"
+               )
     ],
     [
-        psg.Button(key="-Calculate-",button_text="Calculate")
+        psg.Button(key="-Calculate-",
+                   button_text="Calculate"
+                   )
+    ],
+    [
+        psg.Text(key="-Tout-")
     ]
 ]
 
@@ -29,7 +40,15 @@ while True:
 
     if event == "-Calculate-":
         formular = values["-DMG_Input-"]
+        calc = CalculateDamage()
+        try:
+            calc.parse_values(formular)
+        except (ValueError, NameError):
+            print("Could not parse input string")
 
+        dmg = calc.calculate()
+
+        main["-Tout-"].update(f"Calculated Damage:\nAvg: {dmg[0]}\nMin: {dmg[1]}\nMax: {dmg[2]}")
 
     # Close Application if window is closed
     if event == psg.WINDOW_CLOSED:
